1d0111ec3f32265ba14065abd675be3ef6bf777d
s361563 <s361563@studenti.polito.it>
2026-02-24 17:08:55 +0100

    add comment to clarify delay_routine function purpose

--- delay.c
+++ delay.c
@@ -2,6 +2,7 @@
 #include "delay.h"
 #include "task.h"
 
+// This function will block the calling task for the specified number of milliseconds
 void delay_routine(unsigned int milliseconds) {
 
     TickType_t target = xTaskGetTickCount() + pdMS_TO_TICKS(milliseconds);
